72e2cfa9ab21411200f045a4d30211f1c57e7196
Timothy Harris <tim@automated-analytics.com>
2022-06-06 22:01:43 +1000

    Clean up trackTemplate gui. add gooey to requirements.txt. removes some useless hidden files

--- src/mantavision_gui.py
+++ src/mantavision_gui.py
@@ -5,10 +5,11 @@ Example program to demonstrate Gooey's presentation of subparsers
 from typing import Dict
 from gooey import Gooey, GooeyParser
 from mantavision import runTrackTemplate
+from morphology import computeMorphologyMetrics
 
 
-def runConfigForTracking(args) -> Dict:
-    ''' converts the input args from gooey into a dictionary required by the trackTemplate function.'''
+def runTracking(args):
+    ''' Runs TrackTemplate function with the arguments provided by the Gooey UI.'''
     if args.output_frames == 'Yes':
         output_frames = True
     else:
@@ -26,36 +27,47 @@ def runConfigForTracking(args) -> Dict:
     else:
         horizontal_contraction_direction = 0
     contraction_vector = (horizontal_contraction_direction, vertical_contraction_vector)
-    return {
-        'input_video_path': args.video,
-        'contraction_vector': contraction_vector,
-        'output_frames': output_frames,
-        'template_guide_image_path': args.template,
-        'output_video_path': args.output,
-        'guide_match_search_seconds': args.guide_match_search_seconds,
-        'max_translation_per_frame': args.max_translation_per_frame,
-        'max_rotation_per_frame': args.max_rotation_per_frame,
-        'output_conversion_factor': args.output_conversion_factor,
-        'microns_per_pixel': args.microns_per_pixel,
-        'sub_pixel_search_increment': args.sub_pixel_search_increment,
-        'sub_pixel_refinement_radius': args.sub_pixel_refinement_radius
-    }
+    runTrackTemplate(
+        {
+            'input_video_path': args.video,
+            'contraction_vector': contraction_vector,
+            'output_frames': output_frames,
+            'template_guide_image_path': args.template,
+            'output_video_path': args.output,
+            'guide_match_search_seconds': args.guide_match_search_seconds,
+            'max_translation_per_frame': args.max_translation_per_frame,
+            'max_rotation_per_frame': args.max_rotation_per_frame,
+            'output_conversion_factor': args.output_conversion_factor,
+            'microns_per_pixel': args.microns_per_pixel,
+            'sub_pixel_search_increment': args.sub_pixel_search_increment,
+            'sub_pixel_refinement_radius': args.sub_pixel_refinement_radius
+        }
+    )    
 
-def runConfigForMorphology(args) -> Dict:
-    return {}
-
-@Gooey(optional_cols=2, program_name="Curibio Mantavision Toolkit")
-def main():
-    settings_msg = 'Track objects in videos/images'
-    parser = GooeyParser(description=settings_msg)
-    parser.add_argument(
-        '--verbose',
-        help='be verbose',
-        dest='verbose',
-        action='store_true',
-        default=False
+def runMorphology(args):
+    ''' Runs Morphology function with the arguments provided by the Gooey UI.'''    
+    computeMorphologyMetrics(
+        {
+            # search_image_path=test_image_suite_dir,
+            # left_template_image_path=left_template_image_path,
+            # right_template_image_path=right_template_image_path,
+            # template_refinement_radius=40,
+            # edge_finding_smoothing_radius=10,
+            # microns_per_pixel=1.0,    
+            # display_result_images=True,
+            # write_result_images=True,            
+        }
     )
 
+@Gooey(
+    program_name="Curibio Mantavision Toolkit",
+    fullscreen=True,
+    # navigation='TABBED',
+    optional_cols=2
+)
+def main():
+    program_description = 'Track objects in videos/images'
+    parser = GooeyParser(description=program_description)
     subs = parser.add_subparsers(help='Actions', dest='actions')
     track_template_parser = subs.add_parser(
         'Track',
@@ -63,30 +75,41 @@ def main():
     )
     track_template_parser.add_argument(
         'video',
+        metavar='Input Video Path',
         help='path to a directory with the input videos',
-        widget='DirChooser',        
-        type=str
-    )
-    track_template_parser.add_argument(
-        'output_frames',
-        help=' Ouput individual tracking results frames',
-        choices=['Yes', 'No'],
-        default='No'
+        widget='DirChooser',
+        type=str,
+        gooey_options={'full_width': True}
     )
     track_template_parser.add_argument(
         'horizontal_contraction_direction',
+        metavar='Horizontal Contraction Direction',        
         help='Horizontal direction component of contration',
         choices=['left', 'right', 'none'],
         default='right'
     )
     track_template_parser.add_argument(
         'vertical_contraction_direction',
+        metavar='Vertical Contraction Direction',
         help='Vertical direction component of contration',
         choices=['up', 'down', 'none'],
         default='none'
     )    
+    # track_template_parser.add_argument(
+    #     'output_frames',
+    #     help=' Ouput individual tracking results frames',
+    #     choices=['Yes', 'No'],
+    #     default='No'
+    # )
+    track_template_parser.add_argument(
+        '--output_frames',
+        metavar='Output Frames',
+        help=' Ouput individual tracking results frames',
+        action='store_true',
+    )    
     track_template_parser.add_argument(
         '--template',
+        metavar='Template Path',        
         help='path to an image with to be used as a template to track [Leave blank to draw a ROI.]',
         widget='FileChooser',
         type=str,        
@@ -94,6 +117,7 @@ def main():
     )    
     track_template_parser.add_argument(
         '--output', 
+        metavar='Output Path',
         help='dir path to store output of trcked video/s [Leave blank to leave results in sub dir of input.]',
         widget='DirChooser',
         type=str,
@@ -101,42 +125,49 @@ def main():
     )
     track_template_parser.add_argument(
         '--guide_match_search_seconds',
+        metavar='Guide Search Time',
         help='seconds of video to search for a match to the template guide ROI [Leave blank to use the template]',
         type=float,
         default=5.0
     )
     track_template_parser.add_argument(
         '--max_translation_per_frame',
+        metavar='Max Translation', 
         help='limits search to this amount of translation in any direction. [Leave blank to search the entire frame]',
         type=float,
         default=100
     )
     track_template_parser.add_argument(
         '--max_rotation_per_frame',
+        metavar='Max Rotation',        
         help='limits search to this amount of rotation in either direction. [Leave blank to not search with rotation]',
         type=float,
         default=3.0
     )
     track_template_parser.add_argument(
         '--output_conversion_factor',
+        metavar='Conversion Factor',        
         help='will apply this multiplier to all distance calulations',
         type=float,
         default=1.0
     )
     track_template_parser.add_argument(
         '--microns_per_pixel',
+        metavar='Microns per Pixel',        
         help='conversion from pixels to microns for distances in results',
         type=float,
         default=1.0
     )
     track_template_parser.add_argument(
         '--sub_pixel_search_increment',
+        metavar='Subpixel Search Increment',        
         help='search will be sub pixel accurate to within +/- this value',
         type=float,
         default=None
     )
     track_template_parser.add_argument(
         '--sub_pixel_refinement_radius',
+        metavar='Subpixel Refinement Radius',        
         help='sub pixel search will be limited to +/- this amount in each dimension',
         type=float,
         default=None
@@ -160,13 +191,11 @@ def main():
     args = parser.parse_args()
 
     if args.actions == 'Track':
-        run_config = runConfigForTracking(args)
+        runTracking(args)
     elif args.actions == 'Morphology':
-        run_config = runConfigForMorphology(args)
+        runMorphology(args)
     else:
         raise RuntimeError('Invalid Action Chosen')
-
-    runTrackTemplate(run_config)
 
 if __name__ == '__main__':
     main()
